262db20679b9c5cbeff7544572ef0f7495043a32
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-11-15 19:55:43 +0000

    Add test case to showcase interaction with an external api server

--- src/test/java/org/example/testing/AppTests.java
+++ src/test/java/org/example/testing/AppTests.java
@@ -1,7 +1,12 @@
 package org.example.testing;
 
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AppTests {
@@ -11,4 +16,41 @@ class AppTests {
         assertThat(true).isTrue();
     }
 
+    @Nested
+    class InteractionWithExternalApi {
+
+        private ExternalApiServer apiServer = new ExternalApiServer();
+
+        @BeforeEach
+        void setUp() {
+            apiServer.start();
+        }
+
+        @AfterEach
+        void tearDown() {
+            apiServer.stop();
+        }
+
+        @Test
+        void successfulInteraction() {
+            when()
+                .get(apiServer.urlFor("/foo"))
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .assertThat()
+                .body(
+                    equalTo(
+                        """
+                        [
+                            {
+                                "firstName": "Jon",
+                                "lastName": "Doe"
+                            }
+                        ]
+                        """
+                    )
+                );
+        }
+    }
 }
---
+++ src/test/java/org/example/testing/ExternalApiServer.java
@@ -0,0 +1,57 @@
+package org.example.testing;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import java.net.ServerSocket;
+import lombok.SneakyThrows;
+
+class ExternalApiServer {
+
+    private final WireMockServer server;
+
+    ExternalApiServer() {
+        server = new WireMockServer(options().port(randomPort()));
+        server.stubFor(
+            WireMock
+                .get("/foo")
+                .willReturn(
+                    WireMock
+                        .aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                            """
+                            [
+                                {
+                                    "firstName": "Jon",
+                                    "lastName": "Doe"
+                                }
+                            ]
+                            """
+                        )
+                )
+        );
+    }
+
+    public String urlFor(String path) {
+        return server.url(path);
+    }
+
+    public void start() {
+        server.start();
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+    @SneakyThrows
+    private int randomPort() {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            socket.setReuseAddress(true);
+            return socket.getLocalPort();
+        }
+    }
+}
