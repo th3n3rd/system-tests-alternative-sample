a99bd834e0f64885a089b7d78256ef4b62de3c24
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-11-16 09:06:44 +0000

    Move tests for interacting with an external amqp broker into a dedicated test class

--- src/test/java/org/example/testing/AppTests.java
+++ src/test/java/org/example/testing/AppTests.java
@@ -2,10 +2,6 @@ package org.example.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lombok.SneakyThrows;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AppTests {
@@ -15,30 +11,4 @@ class AppTests {
         assertThat(true).isTrue();
     }
 
-    @Nested
-    class InteractionWithExternalAmqpBroker {
-
-        private ExternalAmqpServer amqpServer = new ExternalAmqpServer();
-
-        @BeforeEach
-        void setUp() {
-            amqpServer.start();
-        }
-
-        @AfterEach
-        void tearDown() {
-            amqpServer.stop();
-        }
-
-        @SneakyThrows
-        @Test
-        void successfulInteraction() {
-            amqpServer.givenThereArePeople();
-            var queue = amqpServer.queue("people", "joined");
-            amqpServer.givenThisPersonJoined("Jon", "Doe");
-            var response = amqpServer.receiver().basicGet(queue, true);
-            var message = new String(response.getBody());
-            assertThat(message).isEqualTo("Jon Doe");
-        }
-    }
 }
---
+++ src/test/java/org/example/testing/InteractionsWithExternalAmqpBrokerTests.java
@@ -0,0 +1,36 @@
+package org.example.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@Nested
+class InteractionsWithExternalAmqpBrokerTests {
+
+    private ExternalAmqpServer amqpServer = new ExternalAmqpServer();
+
+    @BeforeEach
+    void setUp() {
+        amqpServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        amqpServer.stop();
+    }
+
+    @SneakyThrows
+    @Test
+    void successfulInteraction() {
+        amqpServer.givenThereArePeople();
+        var queue = amqpServer.queue("people", "joined");
+        amqpServer.givenThisPersonJoined("Jon", "Doe");
+        var response = amqpServer.receiver().basicGet(queue, true);
+        var message = new String(response.getBody());
+        assertThat(message).isEqualTo("Jon Doe");
+    }
+}
