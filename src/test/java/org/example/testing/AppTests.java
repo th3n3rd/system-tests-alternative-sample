b1ba5a20083c180f96f8f61581ffa5c17184b05b
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-11-16 09:05:20 +0000

    Move tests for interacting with an external api server into a dedicated test class

--- src/test/java/org/example/testing/AppTests.java
+++ src/test/java/org/example/testing/AppTests.java
@@ -1,8 +1,6 @@
 package org.example.testing;
 
-import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
 import com.mongodb.client.model.Filters;
 import lombok.SneakyThrows;
@@ -16,46 +14,6 @@ class AppTests {
     @Test
     void tautology() {
         assertThat(true).isTrue();
-    }
-
-    @Nested
-    class InteractionWithExternalApi {
-
-        private ExternalApiServer apiServer = new ExternalApiServer();
-
-        @BeforeEach
-        void setUp() {
-            apiServer.start();
-        }
-
-        @AfterEach
-        void tearDown() {
-            apiServer.stop();
-        }
-
-        @Test
-        void successfulInteraction() {
-            apiServer.givenThisPersonExists("Jon", "Doe");
-
-            when()
-                .get(apiServer.urlFor("/foo"))
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .assertThat()
-                .body(
-                    equalTo(
-                        """
-                        [
-                            {
-                                "firstName": "Jon",
-                                "lastName": "Doe"
-                            }
-                        ]
-                        """
-                    )
-                );
-        }
     }
 
     @Nested
---
+++ src/test/java/org/example/testing/InteractionsWithExternalApiTests.java
@@ -0,0 +1,41 @@
+package org.example.testing;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@Nested
+class InteractionsWithExternalApiTests {
+
+    private ExternalApiServer apiServer = new ExternalApiServer();
+
+    @BeforeEach
+    void setUp() {
+        apiServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        apiServer.stop();
+    }
+
+    @Test
+    void successfulInteraction() {
+        apiServer.givenThisPersonExists("Jon", "Doe");
+
+        when().get(apiServer.urlFor("/foo")).then().assertThat().statusCode(200).assertThat().body(equalTo("""
+                    [
+                        {
+                            "firstName": "Jon",
+                            "lastName": "Doe"
+                        }
+                    ]
+                    """
+                )
+            );
+    }
+}
