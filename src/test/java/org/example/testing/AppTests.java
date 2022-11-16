a5bd29b64f6b056fe13fe949b569a845af464323
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-11-16 08:04:56 +0000

    Add test case to showcase interaction with an external amqp server

--- src/test/java/org/example/testing/AppTests.java
+++ src/test/java/org/example/testing/AppTests.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.mongodb.client.model.Filters;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -78,6 +79,33 @@ class AppTests {
             var people = mongoDbServer.collection("people");
             var jon = people.find(Filters.eq("firstName", "Jon")).first();
             assertThat(jon.get("lastName")).isEqualTo("Doe");
+        }
+    }
+
+    @Nested
+    class InteractionWithExternalAmqpBroker {
+
+        private ExternalAmqpServer amqpServer = new ExternalAmqpServer();
+
+        @BeforeEach
+        void setUp() {
+            amqpServer.start();
+        }
+
+        @AfterEach
+        void tearDown() {
+            amqpServer.stop();
+        }
+
+        @SneakyThrows
+        @Test
+        void successfulInteraction() {
+            amqpServer.givenThereArePeople();
+            var queue = amqpServer.queue("people", "joined");
+            amqpServer.givenThisPersonJoined("Jon", "Doe");
+            var response = amqpServer.receiver().basicGet(queue, true);
+            var message = new String(response.getBody());
+            assertThat(message).isEqualTo("Jon Doe");
         }
     }
 }
---
+++ src/test/java/org/example/testing/ExternalAmqpServer.java
@@ -0,0 +1,100 @@
+package org.example.testing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import lombok.SneakyThrows;
+import org.apache.qpid.server.SystemLauncher;
+import org.apache.qpid.server.util.urlstreamhandler.classpath.Handler;
+
+class ExternalAmqpServer {
+
+    private final SystemLauncher systemLauncher;
+    private final ConnectionFactory factory;
+    private Channel publisher;
+    private Channel receiver;
+
+    static {
+        URL.setURLStreamHandlerFactory(protocol ->
+            "classpath".equals(protocol) ? new Handler() : null
+        );
+    }
+
+    @SneakyThrows
+    public ExternalAmqpServer() {
+        systemLauncher = new SystemLauncher();
+        factory = new ConnectionFactory();
+        factory.setUri(connectionString());
+    }
+
+    @SneakyThrows
+    public void start() {
+        systemLauncher.startup(systemConfig());
+    }
+
+    public void stop() {
+        systemLauncher.shutdown();
+    }
+
+    @SneakyThrows
+    public String queue(String exchange, String routingKey) {
+        var channel = receiver();
+        var queue = channel.queueDeclare().getQueue();
+        channel.queueBind(queue, exchange, routingKey);
+        return queue;
+    }
+
+    @SneakyThrows
+    private Channel publisher() {
+        if (publisher == null) {
+            publisher = factory.newConnection().createChannel();
+        }
+        return publisher;
+    }
+
+    @SneakyThrows
+    public Channel receiver() {
+        if (receiver == null) {
+            receiver = factory.newConnection().createChannel();
+        }
+        return receiver;
+    }
+
+    @SneakyThrows
+    public void givenThereArePeople() {
+        publisher().exchangeDeclare("people", "topic", true);
+    }
+
+    @SneakyThrows
+    public void givenThisPersonJoined(String firstName, String lastName) {
+        publisher()
+            .basicPublish(
+                "people",
+                "joined",
+                null,
+                "%s %s".formatted(firstName, lastName)
+                    .getBytes(StandardCharsets.UTF_8)
+            );
+    }
+
+    private String connectionString() {
+        return "amqp://guest:guest@localhost:5672/default";
+    }
+
+    @SneakyThrows
+    private Map<String, Object> systemConfig() {
+        var initialConfig =
+            ExternalAmqpServer.class.getClassLoader()
+                .getResource("qpid-config.json");
+        return Map.of(
+            "type",
+            "Memory",
+            "initialConfigurationLocation",
+            initialConfig.toExternalForm(),
+            "startupLoggedToSystemOut",
+            true
+        );
+    }
+}
